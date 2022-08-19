ecab2d07b398f8243c9b926ce647100ac3c2f5c9
SofienM <mzabisofien@gmail.com>
2022-08-19 12:39:04 +0200

    Initial commit

---
+++ B_javascript/programmation.js
@@ -0,0 +1,19 @@
+// add(a, b)
+// mult(a, b)
+// increment(a, b)
+// max(a, b)
+
+// minToSecond(2) // 120
+// isEven(2) // true
+// isDivisible(100, 10) // true
+// saison(1) // "Printemps"
+
+// contient(liste, b)
+// contientPas(liste, b)
+// arraySum([1, 2, 3, 4, 5]) // 15
+// compareList([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]) // true
+// compareHeures([11, 22, 03], [11, 21, 03]) // true
+
+// printSuite(10) // 0 1 2 3 4 5 6 7 8 9 10
+// printSuite(1, 10) // 1 2 3 4 5 6 7 8 9 10
+// fooBar(10) // 1 2 'foo' 4 5 'foo' 'bar' 8 9 10 ... 20 'foobar' 22
