0d4a3ab0c3d603e8edcc5db85c52ea1266ebcdf6
SofienM <mzabisofien@gmail.com>
2022-10-18 14:50:24 +0200

    October saving

--- B_javascript/1_programmation.js
+++ B_javascript/1_programmation.js
@@ -17,3 +17,9 @@
 // printSuite(10) // 0 1 2 3 4 5 6 7 8 9 10
 // printSuite(1, 10) // 1 2 3 4 5 6 7 8 9 10
 // fooBar(10) // 1 2 'foo' 4 5 'foo' 'bar' 8 9 10 ... 20 'foobar' 22
+
+// https://www.w3resource.com/python-exercises/python-functions-exercises.php
+// nbMajuscule("Bonjour") // 1
+// nbMajusculeMinuscule("Bonjour") // {"maj": 1, "min": 6}
+// unique([1, 2, 3, 3, 3, 3, 4, 5]) // [1, 2, 3, 4, 5]
+// filtrePairs([1,2,3,4,5,6,7,8,9]) // [2, 4, 6, 8]
