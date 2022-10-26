281aeec4a9a63f2e00af900c1f9c64c54a1da415
SofienM <mzabisofien@gmail.com>
2022-10-26 16:11:23 +0200

    feat(bootstrap): add more bootstrap full sites exo

--- B_javascript/1_programmation.js
+++ B_javascript/1_programmation.js
@@ -2,7 +2,7 @@
 // mult(a, b)
 // max(a, b)
 
-// minToSecond(2) // 120
+// minutesToSecond(2) // 120
 // isEven(2) // true
 // isDivisible(100, 10) // true
 // saison(1) // "Printemps"
@@ -11,7 +11,6 @@
 // contientPas(liste, b)
 // arraySum([1, 2, 3, 4, 5]) // 15
 // compareList([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]) // true
-// compareHeures([11, 22, 03], [11, 21, 03]) // true
 
 // printSuite(10) // 0 1 2 3 4 5 6 7 8 9 10
 // printSuite(1, 10) // 1 2 3 4 5 6 7 8 9 10
@@ -22,3 +21,6 @@
 // nbMajusculeMinuscule("Bonjour") // {"maj": 1, "min": 6}
 // unique([1, 2, 3, 3, 3, 3, 4, 5]) // [1, 2, 3, 4, 5]
 // filtrePairs([1,2,3,4,5,6,7,8,9]) // [2, 4, 6, 8]
+
+// Reste
+// compareHeures([11, 22, 03], [11, 21, 03]) // true
